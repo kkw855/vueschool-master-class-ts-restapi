9d47fe735bc6351afa04a7147e3fd1b254748330
EndSouL <kkw855@gmail.com>
2023-01-10 15:29:32 +0900

    #1 - 순수 함수형 lint 설치 및 설정
    lint 없이도 순수 함수형을 구현할 수도 있지만 너무 많이 반복되는 boilerplate 코드로 인해서
    가독성이 떨어지기 때문에 순수 함수형 프로그래밍을 도와주는 lint 를 설치한다.
    
    * https://github.com/eslint-functional/eslint-plugin-functional

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,11 +3,13 @@ require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
   root: true,
+  plugins: ["functional"],
   extends: [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
     "@vue/eslint-config-typescript",
     "@vue/eslint-config-prettier",
+    "plugin:functional/recommended",
   ],
   parserOptions: {
     ecmaVersion: "latest",
