578c048bd59cbb927131044708f81260144e60f9
EndSouL <kkw855@gmail.com>
2023-01-11 14:39:05 +0900

    #6 - Thread id 에 해당하는 Thread 정보 출력 페이지 만들기 :sparkles:

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,19 @@
 import { createRouter, createWebHistory } from "vue-router";
 import type { RouteRecordRaw } from "vue-router";
 import PageHome from "@/views/PageHome.vue";
+import PageThreadShow from "@/views/PageThreadShow.vue";
 
 const routes: readonly RouteRecordRaw[] = [
   {
     path: "/",
     name: "Home",
     component: PageHome,
+  },
+  {
+    path: "/thread/:id",
+    name: "ThreadShow",
+    component: PageThreadShow,
+    props: true,
   },
 ];
 
