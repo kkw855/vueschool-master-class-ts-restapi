58c59169c0f597240c008a46c11068a3df39161f
EndSouL <kkw855@gmail.com>
2023-01-12 19:02:56 +0900

    #8 - 포스트 에디터 컴포넌트 만들기 :art:

--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -4,24 +4,8 @@
       {{ thread.title }}
     </h1>
 
-    <PostList :posts="threadPosts" />
-    <div class="col-full">
-      <form @submit.prevent="addPost">
-        <div class="form-group">
-          <textarea
-            v-model="newPostData"
-            name=""
-            id=""
-            cols="30"
-            rows="10"
-            class="form-input"
-          />
-        </div>
-        <div class="form-actions">
-          <button class="btn-blue">Submit post</button>
-        </div>
-      </form>
-    </div>
+    <post-list :posts="threadPosts" />
+    <post-editor @save="save" />
   </div>
 </template>
 
@@ -31,6 +15,8 @@ import sourceData from "@/data.json";
 import type Post from "@/types/Post";
 import type Thread from "@/types/Thread";
 import { computed, reactive, ref } from "vue";
+import PostEditor from "@/components/PostEditor.vue";
+import type PostSaveEvent from "@/types/PostSaveEvent";
 
 // props
 const props = defineProps<{
@@ -38,28 +24,24 @@ const props = defineProps<{
 }>();
 
 // data
-const threads: Thread[] = sourceData.threads;
+const threads = reactive<Thread[]>(sourceData.threads);
 // JSON 파일에 포함된 emoji 를 위해서
 const posts = reactive<Post[]>(JSON.parse(JSON.stringify(sourceData.posts)));
 const thread = computed(() => threads.find((t) => t.id === props.id));
 const threadPosts = computed(() =>
   posts.filter((p) => p.threadId === props.id)
 );
-const newPostData = ref("");
 
 // methods
-function addPost() {
-  const postId = "gggg" + Math.random();
+function save(eventData: PostSaveEvent) {
   const post: Post = {
-    id: postId,
-    text: newPostData.value,
-    publishedAt: Math.floor(Date.now() / 1000),
+    ...eventData.post,
     threadId: props.id,
-    userId: "7uVPJS9GHoftN58Z2MXCYDqmNAh2",
   };
-  posts.push(post);
-  thread.value.posts.push(postId);
 
-  newPostData.value = "";
+  console.log(post);
+
+  thread.value.posts.push(post.id);
+  posts.push(post);
 }
 </script>
